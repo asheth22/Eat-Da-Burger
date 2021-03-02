07f7fd91f42c5855f6125c0934fc4381004ad917
Anjali Sheth <anjali.sheth@otis.com>
2021-03-02 13:24:51 -0500

    npm packages installed and db created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Single_Melt', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon_Melt', true);
+INSERT INTO burgers (burger_name) VALUES ('Impossible');
+INSERT INTO burgers (burger_name) VALUES ('Rodeo');
